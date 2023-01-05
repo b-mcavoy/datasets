1302e091ea320b71f8502d4765b4843dc8d68ee5
b-mcavoy <111782536+b-mcavoy@users.noreply.github.com>
2023-01-05 11:52:51 -0500

    Add files via upload

---
+++ utils (1).js
@@ -0,0 +1,26 @@
+
+function getColumn(url, columnNumber){
+  var column = [];
+  var table = [];
+  var request = new XMLHttpRequest();  
+  request.open("GET", url, false);   
+  request.send(null);  
+  var csvData = new Array();
+  var jsonObject = request.responseText.split(/\r?\n|\r/);
+  for (var i = 0; i < jsonObject.length; i++) {
+    csvData.push(jsonObject[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/));
+  }
+  table = csvData;
+  column = getCol(table, columnNumber);
+  return column;
+}
+
+//returns the specified column from a 2D Array
+function getCol(matrix, col){
+       var column = [];
+       for(var i=1; i<matrix.length-1; i++){
+          column.push(matrix[i][col]);
+       }
+       return column;
+    }
+
